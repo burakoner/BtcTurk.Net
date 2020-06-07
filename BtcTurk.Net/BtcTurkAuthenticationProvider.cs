7a5f0304a9807608000203cbab6f18d5020269b2
Burak Öner <info@burakoner.com>
2020-06-07 13:45:37 +0300

    Update BtcTurkAuthenticationProvider.cs

--- BtcTurk.Net/BtcTurkAuthenticationProvider.cs
+++ BtcTurk.Net/BtcTurkAuthenticationProvider.cs
@@ -1,4 +1,4 @@
-﻿using CryptoExchange.Net;
+using CryptoExchange.Net;
 using CryptoExchange.Net.Authentication;
 using CryptoExchange.Net.Objects;
 using System;
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
+using System.Net.Http;
+using System.Web;
 
 namespace BtcTurk.Net
 {
@@ -17,57 +19,48 @@ namespace BtcTurk.Net
 
         public BtcTurkAuthenticationProvider(ApiCredentials credentials, ArrayParametersSerialization arraySerialization) : base(credentials)
         {
+            if (credentials.Secret == null)
+                throw new ArgumentException("No valid API credentials provided. Key/Secret needed.");
+
             encryptor = new HMACSHA256(Encoding.ASCII.GetBytes(credentials.Secret.GetString()));
             this.arraySerialization = arraySerialization;
         }
-        /*
-        public override Dictionary<string, object> AddAuthenticationToParameters(string uri, string method, Dictionary<string, object> parameters, bool signed)
+
+        public override Dictionary<string, string> AddAuthenticationToHeaders(string uri, HttpMethod method, Dictionary<string, object> parameters, bool signed)
         {
-            if (!signed)
-                return parameters;
+            if (Credentials.Key == null || Credentials.Secret==null)
+                throw new ArgumentException("No valid API credentials provided. Key/Secret needed.");
 
-            /*
-            var query = parameters.CreateParamString(true, arraySerialization);
-            parameters.Add("signature", ByteToString(encryptor.ComputeHash(Encoding.UTF8.GetBytes(query))));
-            * /
-
-            // Dummy Return
-            return parameters;
-        }
-
-        public override Dictionary<string, string> AddAuthenticationToHeaders(string uri, string method, Dictionary<string, object> parameters, bool signed)
-        {
             if (uri.Contains("/v1/"))
             {
                 var sign = string.Empty;
                 var apiKey = Credentials.Key.GetString();
-                var unixTime = DateTime.UtcNow.ToUnixTimeMilliSeconds();
-
-                using (HMACSHA256 hmac = new HMACSHA256(Convert.FromBase64String(Credentials.Secret.GetString())))
+                var apiSecret = Credentials.Secret.GetString();
+                var nonce = DateTime.UtcNow.ToUnixTimeMilliSeconds();
+                string message = apiKey + nonce;
+                using (HMACSHA256 hmac = new HMACSHA256(Convert.FromBase64String(apiSecret)))
                 {
-                    byte[] signatureBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(apiKey + unixTime));
+                    byte[] signatureBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(message));
                     sign = Convert.ToBase64String(signatureBytes);
                 }
 
                 return new Dictionary<string, string> {
                 { "X-PCK", Credentials.Key.GetString() },
-                { "X-Stamp", unixTime.ToString() },
+                { "X-Stamp", nonce.ToString() },
                 { "X-Signature", sign },
             };
             }
-            else if (uri.Contains("/v1/"))
+            else if (uri.Contains("/v3/"))
             {
                 return new Dictionary<string, string>();
             }
 
-            // Dummy Return
             return new Dictionary<string, string>();
         }
-        */
+
         public override string Sign(string toSign)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
-
     }
 }
